c439ed3fe9a02cc6a16ea918d793ed49a27e82e2
Lidor Bashari <lidor1965@gmail.com>
2024-06-25 22:16:33 +0300

    project one

--- yourSolution.sh
+++ yourSolution.sh
@@ -1,3 +1,9 @@
-sudo chmod 600 secretDir/.secret
+mkdir secretDir
+rm maliciousFiles/amIMaliciousOrNot.WhoKnows
+rm maliciousFiles/somefileIsLinkingToMe.BeAware
+rm -fr maliciousFiles/
+touch secretDir/.secret
+chmod 600 secretDir/.secret
+rm important.link
 /bin/bash generateSecret.sh
-cat ~/src/secretDir/.secret
+cat secretDir/.secret
