28a821edbc363215d37f345560297e0152623705
Lidor Bashari <lidor1965@gmail.com>
2024-06-25 19:45:16 +0300

    project one

--- yourSolution.sh
+++ yourSolution.sh
@@ -1,5 +1,12 @@
 
 wget https://alonitac.github.io/DevOpsTheHardWay/linux_project/secretGenerator.tar.gz
 tar -zxvf secretGenerator.tar.gz
-cd
-  /bin/bash generateSecret.sh
+cd src
+mkdir secretDir
+cd secretDir/
+touch .secret
+cd ~/src
+chmod 600 secretDir/.secret
+rm -r maliciousFiles/
+rm important.link
+/bin/bash generateSecret.sh
