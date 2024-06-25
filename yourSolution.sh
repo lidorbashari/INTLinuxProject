7a1643e6e984ed9eefb2353526c53b85fc8116e5
Lidor Bashari <lidor1965@gmail.com>
2024-06-25 19:27:59 +0300

    project one

--- yourSolution.sh
+++ yourSolution.sh
@@ -1,9 +1,14 @@
+
+ cd src
+mkdir secrtDir
+rm -r maliciousFiles/
+cd
+cd src
+cd secretDir/
+touch .secret
+chmod 600 secretDir/.secret
+cd ~/src
+chmod 600 secretDir/.secret
+cd ~/src
+chmod 600 secretDir/.secret
 /bin/bash generateSecret.sh
-
-
-secret=$(cat secretDir/.secret)
-
-
-echo "the generate srcret is -" $secret
-
-
